16ebde0b1825fb00820769815241711342144111
gagandeep22 <gagandeep.u@somaiya.edu>
2025-07-07 14:26:05 +0530

    app done

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:bmi_app/screen_splash.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
